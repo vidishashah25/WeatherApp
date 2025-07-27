3c93ac779485c4eaa48ada63454bb431cf59e1b7
Vidisha Shah <shahvidisha1996@gmail.com>
2025-07-27 22:25:49 +0200

    Blazor app, EF Core, BCrypt, MySQL cloud connection, initial migration for database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherApp.Models;
+
+namespace WeatherApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using System;
+using WeatherApp.Data;
 using WeatherApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString =
+    $"Server=mysql-139b5804-weather-app-fcs.k.aivencloud.com;Port=25934;Database=blazor_weather_db;Uid=avnadmin;Pwd={dbPassword};SslMode=Required;";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)))
+);
+
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
