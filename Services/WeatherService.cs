f2407553148021a24da07910ce07873174787c2c
Vidisha Shah <shahvidisha1996@gmail.com>
2025-08-01 13:47:45 +0200

    changed api current to forecast

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -22,7 +22,7 @@ namespace WeatherApp.Services
             if (string.IsNullOrWhiteSpace(city))
                 throw new ArgumentException("City is required", nameof(city));
 
-            var url = $"https://api.weatherapi.com/v1/current.json?key={_apiKey}&q={city}";
+            var url = $"https://api.weatherapi.com/v1/forecast.json?key={_apiKey}&q={city}&days=3";
             var response = await _httpClient.GetAsync(url);
 
             var body = await response.Content.ReadAsStringAsync();
@@ -38,6 +38,7 @@ namespace WeatherApp.Services
     {
         public Location location { get; set; }
         public Current current { get; set; }
+        public Forecast forecast { get; set; }
 
         public class Location
         {
@@ -45,6 +46,7 @@ namespace WeatherApp.Services
             public string country { get; set; }
             public string localtime { get; set; }
         }
+
         public class Current
         {
             public float temp_c { get; set; }
@@ -53,6 +55,26 @@ namespace WeatherApp.Services
             public Condition condition { get; set; }
             public float feelslike_c { get; set; }
         }
+
+        public class Forecast
+        {
+            public List<ForecastDay> forecastday { get; set; }
+        }
+
+        public class ForecastDay
+        {
+            public string date { get; set; }
+            public Day day { get; set; }
+        }
+
+        public class Day
+        {
+            public float maxtemp_c { get; set; }
+            public float mintemp_c { get; set; }
+            public float avgtemp_c { get; set; }
+            public Condition condition { get; set; }
+        }
+
         public class Condition
         {
             public string text { get; set; }
